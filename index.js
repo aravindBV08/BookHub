291fcb5fb31fb49a1f0ed5f649d513d764c9dab2
Aravind <aravindbvadavalliki@gmail.com>
2025-11-26 18:41:19 +0530

    SDfdsfds

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
-import path from "path";
+const path = require( "path");
 dotenv.config();
 const app = express();
 
