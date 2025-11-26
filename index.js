b0a843dd54d0847516d2783f0ddcadc9d68205b5
Aravind <aravindbvadavalliki@gmail.com>
2025-11-26 18:40:28 +0530

    SDfdsfdsFsadsadfa

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
-
+import path from "path";
 dotenv.config();
 const app = express();
 
